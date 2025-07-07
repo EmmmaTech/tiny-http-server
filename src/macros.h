59f629f1fbe7c80e5dfccdfbbbcef9d51040deca
Emma Terzioglu <emreterzioglu49@gmail.com>
2025-07-06 21:56:28 -0700

    feat: new macros :D

--- src/macros.h
+++ src/macros.h
@@ -6,8 +6,9 @@
 #define STRINGS_EQUAL(x, y) (strcmp(x, y) == 0)
 #define STRINGS_EQUAL_N(x, y, n) (strncmp(x, y, n) == 0)
 
-#define SCUFFED_MEMCPY(dest, src, len) \
-                                        for (i = 0; i < len; i++) { dest[i] = *src; src++; }; \
-                                        dest[i] = 0;
+#define LEN(x) ((sizeof(x)) / ((sizeof(x[0]))))
+
+#define MIN(x, y) ((x < y) ? (x) : (y))
+#define MAX(x, y) ((x > y) ? (x) : (y))
 
 #endif
